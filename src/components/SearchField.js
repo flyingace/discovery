2f58029e6b6ca0880eb9c17fd1eacf0b95d98fd1
flyingace <dc@davidcameron.biz>
2017-08-28 01:36:11 -0400

    Following search or sort first video in list is now featured

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Form, FormControl } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { trim } from 'lodash';
 
 class AppActions extends React.Component {
 
@@ -9,18 +10,24 @@ class AppActions extends React.Component {
     };
 
     onSearchClick = () => {
-        this.props.fetchVideo(this.searchInput.value);
+        this.searchInput.value = trim(this.searchInput.value);
+
+        if (this.searchInput.value !== '') {
+            this.props.fetchVideo(this.searchInput.value);
+        } else {
+            window.alert ('Please enter a term to search by.');
+        }
     };
 
     render() {
         return (
+            // TODO: If you have time, validate entry isn't empty or just spaces
             <Form inline>
                 <FormControl
                     componentClass="input"
                     placeholder="Search For Videos"
                     inputRef={(ref) => {this.searchInput = ref}}
-                    defaultValue="Dogs"
-                    //TODO: If you have time, validate entry isn't empty
+                    // defaultValue="Dogs"
                 />
                 <Button onClick={this.onSearchClick}>Search Videos</Button>
             </Form>
